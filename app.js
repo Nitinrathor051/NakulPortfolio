7635e36accaf7a6a94219f2dd096cc788eb7f4c5
Nitin Rathor <nitinrathor0505feaid@gmail.com>
2026-02-12 12:41:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
 {
   mediaType: 'video',
   aspectRatio: 'ar-horizontal',
-  videoUrl: "https://res.cloudinary.com/dpdhh76ey/video/upload/v1770874542/HairstickyLandscape_u2stzf.mp4",
+  videoUrl: "https://res.cloudinary.com/dpdhh76ey/video/upload/v1770879950/%E0%A4%B0%E0%A4%BE%E0%A4%A3%E0%A4%BE_%E0%A4%B8%E0%A4%BE%E0%A4%82%E0%A4%97%E0%A4%BE_%E0%A4%B5%E0%A4%BF%E0%A4%B5%E0%A4%BE%E0%A4%A6_%E0%A4%AE%E0%A5%87%E0%A4%82_%E0%A4%A6%E0%A4%BF%E0%A4%97%E0%A5%8D%E0%A4%B5%E0%A4%BF%E0%A4%9C%E0%A4%AF_%E0%A4%B8%E0%A4%BF%E0%A4%82%E0%A4%B9_%E0%A4%95%E0%A5%80_%E0%A4%8F%E0%A4%82%E0%A4%9F%E0%A5%8D%E0%A4%B0%E0%A5%80___%E0%A4%B8%E0%A4%BE%E0%A4%82%E0%A4%B8%E0%A4%A6_%E0%A4%95%E0%A5%80_%E0%A4%9F%E0%A4%BF%E0%A4%AA%E0%A5%8D%E0%A4%AA%E0%A4%A3%E0%A5%80_%E0%A4%AA%E0%A4%B0_%E0%A4%AD%E0%A4%A1%E0%A4%BC%E0%A4%95%E0%A5%87_%E0%A4%9C%E0%A4%BE%E0%A4%A8%E0%A4%BF%E0%A4%8F_%E0%A4%95%E0%A5%8D%E0%A4%AF%E0%A4%BE_%E0%A4%95%E0%A4%B9%E0%A4%BE_dogfey.mp4",
   title: 'Hair Care Product Commercial',
   contentType: 'Beauty Brand Advertisement',
   editingStyle: 'Commercial Cinematic Look + Smooth Product Focus Transitions',
